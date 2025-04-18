d7083ca3c7545b2aa5a1ffdad5fea7cafe6fd051
nghiaugust <115493863+nghiaugust@users.noreply.github.com>
2025-04-18 16:54:19 +0700

    init backend

---
+++ BackEnd/yapping/src/main/java/com/yapping/YappingApplication.java
@@ -0,0 +1,13 @@
+package com.yapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YappingApplication.class, args);
+	}
+
+}
